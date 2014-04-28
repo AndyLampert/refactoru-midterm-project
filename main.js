111e74cdd9bdbda7db403b018c50ea9d6a7e44fb
AndyLampert <lamperta@gmail.com>
2014-04-28 15:59:10 -0600

    took out parallax and changed lightbox

--- main.js
+++ main.js
@@ -3,38 +3,45 @@ $(document).ready(function(){
    // cache the window object
    $window = $(window);
  
-   $('section[data-type="background"]').each(function(){
-     // declare the variable to affect the defined data-type
-     var $scroll = $(this);
+   // $('section[data-type="background"]').each(function(){
+   //   // declare the variable to affect the defined data-type
+   //   var $scroll = $(this);
                      
-      $(window).scroll(function() {
-        // HTML5 proves useful for helping with creating JS functions!
-        // also, negative value because we're scrolling upwards                             
-        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+   //    $(window).scroll(function() {
+   //      // HTML5 proves useful for helping with creating JS functions!
+   //      // also, negative value because we're scrolling upwards                          
+        
+   //      // not going to use the 'speed'??
+   //      var yPos = -($window.scrollTop() / $scroll.data('speed')); 
          
-        // background position
-        var coords = '50% '+ yPos + 'px';
+   //      // background position
+   //      var coords = '50% '+ yPos + 'px';
  
-        // move the background
-        $scroll.css({ backgroundPosition: coords });    
+   //      // move the background
+   //      $scroll.css({ backgroundPosition: coords });    
 
-      }); // end window scroll
-   });  // end parallax section
+   //    }); // end window scroll
+   // });  // end parallax section
 
    // registration button click event handler
    $(document).on('click','.registration-btn',function(){
-   		// code
+    	// code
    });
 
-   // close registration event handler 
-   $(document).on('click','.close-lightbox',function(){
-   		// hide the lightbox
-   		$(this).closest('.lightbox-container').hide();
-   });
+    // close registration event handler 
+    $(document).on('click','.close-lightbox-link',function(){
+    	// hide the lightbox
+    	$(this).closest('.lightbox-container').css('display','none');
+    });
 
-   // click through registration click event handler
-   $(document).on('click','.accept-registration',function(){
+    // sign up button click event handler
+    $(document).on('click','.signup-btn',function(){
+    $('.lightbox-container').css('display','inline-block');
+    });
 
-   });
+    // click through registration click event handler
+    $(document).on('click','.accept-registration',function(){
+
+    });
 
 }); // close out script (document ready)
