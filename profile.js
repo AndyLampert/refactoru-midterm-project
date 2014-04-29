257cc386a1670a1ea164c6a13ef3ff5099868b40
AndyLampert <lamperta@gmail.com>
2014-04-29 17:22:09 -0600

    got save/edit buttons working on profile page

--- profile.js
+++ profile.js
@@ -1,4 +1,5 @@
 $(document).on('ready',function(){
+	$('.user-editable-field').addClass('hide');
 	// event handler for editing profile
 	$('.edit-profile-btn').on('click',function(){
 		// 1) change the text to "save profile"
@@ -8,11 +9,17 @@ $(document).on('ready',function(){
 		} else {
 			$(this).text('Edit Profile');
 		}
-		// when content = "Save Changes" & click, change to edit profile
+
+		// SHOW the editable fields when you click the edit button
+		$('.user-editable-field').removeClass('hide'); 
 		
-		// $(this).text('Edit Profile');
-		
-		$('.ready-for-selector').addClass('hide'); 
-	})
+		// when the click the button (when the text is 'save changes')
+		// add the .hide class back to the user-editable-field
+		// var saveChanges = $('edit-profile-btn').text()
+		if($('.edit-profile-btn').text() !== 'Save Changes'){
+			$('.user-editable-field').addClass('hide');
+		}
+
+	});
 
 });
